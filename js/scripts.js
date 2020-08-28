e0f6d761cf1f95382a944e559b87a4348707004d
Rehan Khan <48653396+rehannk@users.noreply.github.com>
2020-08-28 21:30:09 +0530

    first version of website

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+  var typed = new Typed(".animateText", {
+    strings:[
+      "Developer",
+      "Coffee Lover",
+      "Dark Theme Coder"
+    ],
+    typeSpeed:50,
+    backSpeed: 40,
+    loop: true
+  });
+  
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+      $(".goTop").fadeIn();
+    } else {
+      $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
+  });
+
+  $(".goTop").click(function () {
+    scroll(0, 0);
+  });
+
+  $(".menu-toggler").click(function () {
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+
+  $(".works").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: { enabled: true },
+  });
+});
