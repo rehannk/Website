98b5a884a42e303443310ce65a3c104fc681eefb
Rehan Khan <48653396+rehannk@users.noreply.github.com>
2020-08-29 19:55:44 +0530

    Update scripts.js
    Changes in script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,42 @@
 $(document).ready(function () {
-
-  var typed = new Typed(".animateText", {
-    strings:[
-      "Developer",
-      "Coffee Lover",
-      "Dark Theme Coder"
-    ],
-    backSpeed: 60,
-    smartBackspace: true,
-    backDelay: 1000,
-    startDelay: 1000,
-    typeSpeed: 60,
-    loop: true,
+    var typed = new Typed(".animateText", {
+      strings: ["Developer", "Coffee Lover", "Dark Theme Coder"],
+      backSpeed: 60,
+      smartBackspace: true,
+      backDelay: 1000,
+      startDelay: 1000,
+      typeSpeed: 60,
+      loop: true,
+    });
+  
+    $(window).scroll(function () {
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+        $(".goTop").fadeIn();
+      } else {
+        $(".navbar").removeClass("sticky");
+        $(".goTop").fadeOut();
+      }
+    });
+  
+    $(".goTop").click(function () {
+      scroll(0, 0);
+    });
+  
+    $(".menu-toggler").click(function () {
+      $(this).toggleClass("active");
+      $(".navbar-menu").toggleClass("active");
+    });
+  
+    $(".navbar-menu > a").click(function () {
+      $(".menu-toggler").removeClass("active");
+      $(".navbar-menu").removeClass("active");
+    });
+  
+    $(".works").magnificPopup({
+      delegate: "a",
+      type: "image",
+      gallery: { enabled: true },
+    });
   });
   
-  $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-      $(".goTop").fadeIn();
-    } else {
-      $(".navbar").removeClass("sticky");
-      $(".goTop").fadeOut();
-    }
-  });
-
-  $(".goTop").click(function () {
-    scroll(0, 0);
-  });
-
-  $(".menu-toggler").click(function () {
-    $(this).toggleClass("active");
-    $(".navbar-menu").toggleClass("active");
-  });
-  
-  $(".navbar-menu > a").click(function () {
-    $(".menu-toggler").removeClass("active");
-    $(".navbar-menu").removeClass("active");
-  });
-
-  $(".works").magnificPopup({
-    delegate: "a",
-    type: "image",
-    gallery: { enabled: true },
-  });
-});
